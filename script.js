7c8511c5fb710556879383e76780c751d0a98369
Michael Halaj <michaelhalaj7@gmail.com>
2023-01-04 13:12:43 -0500

    Template html and javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(author, title, pages, read) {
+  // the constructor...
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
