045ff9350f31e4983f76170fed544fa351c1c5a6
Michael Halaj <michaelhalaj7@gmail.com>
2023-01-05 12:28:27 -0500

    Add buttons and requirement field to form

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addButton = document.querySelector(".add-book");
 const library = document.querySelector(".library");
 const form = document.querySelector("form");
+const closeButton = document.querySelector(".close");
 const myLibrary = [];
 
 function Book(author, title, pages, read) {
@@ -19,3 +20,6 @@ function addBookToLibrary(author, title, pages, read) {
 addButton.addEventListener("click", () => {
   form.classList.remove("hide");
 });
+closeButton.addEventListener("click", ()=>{
+  form.classList.add("hide");
+})
