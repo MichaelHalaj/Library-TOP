6deddd3fd6985b8ac9fad1d35336d8e8e20ebfab
Michael Halaj <michaelhalaj7@gmail.com>
2023-01-06 15:01:50 -0500

    Add error messages when field is empty

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const library = document.querySelector(".library");
 const form = document.querySelector("form");
 const closeButton = document.querySelector(".close");
 const submit = document.querySelector("#submit");
+const errors = document.querySelectorAll(".error");
 const myLibrary = [];
 
 function Book(author, title, pages, read) {
@@ -18,8 +19,63 @@ function addBookToLibrary(author, title, pages, read) {
   myLibrary.push(new Book(author, title, pages, read));
 }
 function clearForm() {
+  for(let i = 0; i < errors.length; i+=1){
+    errors[i].classList.add("hide");
+  }
   form.classList.add("hide");
   form.reset();
+}
+function validateForm(author, title, pages, read){
+  console.log(author.value === '');
+  console.log(title.value);
+  console.log(pages.value);
+  let hasNoErrors = true;
+  if(author.value === ""){
+    const authorError = document.querySelector('.author-error');
+    authorError.classList.remove('hide');
+    hasNoErrors = false;
+  }
+  if(title.value === ""){
+    const titleError = document.querySelector('.title-error');
+    titleError.classList.remove('hide');
+    hasNoErrors = false;
+  }
+  if(pages.value === ""){
+    const pageError = document.querySelector('.pages-error');
+    pageError.classList.remove('hide');
+    hasNoErrors = false;
+  }
+  return hasNoErrors;
+}
+function createCard(){
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const deleteBook = document.createElement("div");
+  deleteBook.classList.add("delete");
+
+  const content = document.createElement("div");
+  content.classList.add("content");
+
+
+  const title = document.querySelector("#book-title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  const read = document.querySelector("input[name='read-status']:checked");
+  validateForm(author, title, pages, read);
+  addBookToLibrary(author, title, pages, read === "read");
+  
+  const margin = document.createElement("div");
+  margin.classList.add("margin");
+  margin.classList.add(read.id);
+
+  content.innerHTML = `<div>${title.value}</div>
+  <div>${author.value}</div>
+  <div>${pages.value} Pages</div>`;
+  card.append(margin);
+  card.appendChild(content);
+  card.appendChild(deleteBook);
+  library.appendChild(card);
 }
 addButton.addEventListener("click", () => {
   form.classList.remove("hide");
@@ -29,28 +85,6 @@ closeButton.addEventListener("click", () => {
 });
 submit.addEventListener("click", (e) => {
   e.preventDefault();
-  const card = document.createElement("div");
-  card.classList.add("card");
-  const deleteBook = document.createElement("div");
-  deleteBook.classList.add("delete");
-
-  const content = document.createElement("div");
-  content.classList.add("content");
-
-  const margin = document.createElement("div");
-  const title = document.querySelector("#book-title");
-  const author = document.querySelector("#author");
-  const pages = document.querySelector("#pages");
-  const read = document.querySelector("input[name='read-status']:checked");
-  addBookToLibrary(author, title, pages, read === "read");
-  margin.classList.add("margin");
-  margin.classList.add(read.id);
-  content.innerHTML = `<div>${title.value}</div>
-  <div>${author.value}</div>
-  <div>${pages.value} Pages</div>`;
-  card.append(margin);
-  card.appendChild(content);
-  card.appendChild(deleteBook);
-  library.appendChild(card);
+  createCard();
   clearForm();
 });
