c1cd4e058c12c5affcc9b3ab032f037c0ac1f8de
Michael Halaj <michaelhalaj7@gmail.com>
2023-01-06 16:03:20 -0500

    Add book logo and adjust close button

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function addBookToLibrary(author, title, pages, read) {
   // do stuff here
   myLibrary.push(new Book(author, title, pages, read));
 }
-function clearErrors() {
-  for (let i = 0; i < errors.length; i += 1) {
+function clearErrors(){
+  for(let i = 0; i < errors.length; i+=1){
     errors[i].classList.add("hide");
   }
 }
@@ -28,40 +28,37 @@ function clearForm() {
   form.classList.add("hide");
   form.reset();
 }
-function validateForm(author, title, pages, read) {
+function validateForm(author, title, pages, read){
   clearErrors();
   let hasNoErrors = true;
-  if (author.value === "") {
-    const authorError = document.querySelector(".author-error");
-    authorError.classList.remove("hide");
+  if(author.value === ""){
+    const authorError = document.querySelector('.author-error');
+    authorError.classList.remove('hide');
     hasNoErrors = false;
   }
-  if (title.value === "") {
-    const titleError = document.querySelector(".title-error");
-    titleError.classList.remove("hide");
+  if(title.value === ""){
+    const titleError = document.querySelector('.title-error');
+    titleError.classList.remove('hide');
     hasNoErrors = false;
   }
-  if (pages.value === "") {
-    const pageError = document.querySelector(".pages-error");
-    pageError.classList.remove("hide");
+  if(pages.value === ""){
+    const pageError = document.querySelector('.pages-error');
+    pageError.classList.remove('hide');
     hasNoErrors = false;
   }
   return hasNoErrors;
 }
-function removeBook(bookID) {
+function removeBook(bookID){
   myLibrary.splice(bookID, 1);
-  console.log(bookID);
-  console.log(`#${bookID}`);
-  const card = document.querySelector(`#\\3${bookID} `);
-  console.log(card);
+  const card = document.querySelector(`#\\3${bookID} `); // unicode
   library.removeChild(card);
   lastBookIdx -= 1;
-  const newList = document.querySelectorAll(".card");
-  for (let i = 0; i < newList.length; i += 1) {
+  const newList = document.querySelectorAll('.card');
+  for(let i = 0; i < newList.length; i+=1){
     newList[i].id = i;
   }
 }
-function createCard() {
+function createCard(){
   const card = document.createElement("div");
   card.classList.add("card");
   card.id = lastBookIdx;
@@ -77,16 +74,16 @@ function createCard() {
   const author = document.querySelector("#author");
   const pages = document.querySelector("#pages");
   const read = document.querySelector("input[name='read-status']:checked");
-  if (validateForm(author, title, pages, read)) {
+  if(validateForm(author, title, pages, read)){
     addBookToLibrary(lastBookIdx, author, title, pages, read === "read");
     console.table(myLibrary);
-    deleteBook.addEventListener("click", () => {
+    deleteBook.addEventListener('click', ()=>{
       removeBook(card.id);
     });
     const margin = document.createElement("div");
     margin.classList.add("margin");
     margin.classList.add(read.id);
-
+  
     content.innerHTML = `<div>${title.value}</div>
     <div>${author.value}</div>
     <div>${pages.value} Pages</div>`;
@@ -99,6 +96,7 @@ function createCard() {
     lastBookIdx++;
     clearForm();
   }
+
 }
 addButton.addEventListener("click", () => {
   form.classList.remove("hide");
@@ -110,4 +108,5 @@ closeButton.addEventListener("click", () => {
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   createCard();
+
 });
